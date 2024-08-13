eac104986e042c67de7880cf2e03cd8dcfe1f683
Hetu Jotaniya <hetujotaniya24hj@gmail.com>
2024-08-13 20:43:15 +0530

    Initial blank project

---
+++ HJColorPickerViewTask/HJColorPickerViewTask/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  HJColorPickerViewTask
+//
+//  Created by Hetu Jotaniya on 13/08/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ HJColorPickerViewTask/HJColorPickerViewTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HJColorPickerViewTask
+//
+//  Created by Hetu Jotaniya on 13/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
